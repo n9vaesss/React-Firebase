b0abcb027f0c9397a99780ab66bc581ac2f62bf8
n9vaesss <caioo24332433@gmail.com>
2023-06-07 11:22:36 -0300

    criando react firebase

---
+++ firebase/src/App.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import './App.css'
+
+import { db } from './firebaseConnection'
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc } from 'firebase/firestore'
+
+function App() {
+
+  const [titulo, setTitulo] = useState('')
+  const [autor, setAutor] = useState('')
+  const [idPost, setIdPost] = useState('')
+
+  const [posts, setPosts] = useState([])
+
+  async function handleAdd() {
+    // await setDoc(doc(db, "posts", "12345"),{
+    //   titulo: titulo,
+    //   autor: autor
+    // })
+    // .then(()=>{
+    //   console.log("dados registrados no banco")
+    // })
+    // .catch((error)=>{
+    //   console.log("gerou erro" + error)
+    // })
+
+    await addDoc(collection(db, 'posts'), {
+      titulo: titulo,
+      autor: autor
+    })
+      .then(() => {
+        console.log("sucesso")
+        setAutor('')
+        setTitulo('')
+      })
+      .catch((error) => {
+        console.log('Erro' + error)
+      })
+
+  }
+
+  async function buscarPost() {
+    // const postRef = doc(db, "posts", "z9dX5dgtDKlRuHQXWD22")
+
+    // await getDoc(postRef)
+    // .then((snapshot)=>{
+    //   setAutor(snapshot.data().autor)
+    //   setTitulo(snapshot.data().titulo)
+    // })
+    // .catch((error)=>{
+    //   console.log(error)
+    // })
+
+    const postsRef = collection(db, 'posts')
+
+    await getDocs(postsRef)
+      .then((snapshot) => {
+        let lista = []
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor
+          })
+        })
+
+        setPosts(lista)
+
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }
+
+  async function editarPost(){
+    const docRef = doc(db, "posts", idPost)
+
+    await updateDoc(docRef,{
+      titulo: titulo,
+      autor: autor
+    })
+    .then(()=>{
+      setIdPost('')
+      setAutor('')
+      setTitulo('')
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  async function excluirPost(id){
+    const docRef = doc(db, "posts", id)
+
+    await deleteDoc(docRef)
+    .then(()=>{
+      alert("Post deletado com sucesso")
+    })
+  }
+
+  return (
+    <div>
+      <h1>React firebase</h1>
+      <div className="container">
+        <label>Id do Post</label>
+        <input
+        type="text" 
+        placeholder="Digite o Id dio o post"
+        value={idPost}
+        onChange={(e)=> setIdPost(e.target.value)}
+        /> <br />
+        <label>Titulo:</label>
+        <textarea
+          placeholder="Digite um titulo"
+          typeof="text"
+          value={titulo}
+          onChange={(e) => setTitulo(e.target.value)}
+        />
+        <label>Autor</label>
+        <input
+          type="text"
+          placeholder="Autor do post"
+          value={autor}
+          onChange={(e) => setAutor(e.target.value)}
+        />
+        <button onClick={handleAdd}>Cadastrar</button>
+        <button onClick={buscarPost} >Buscar post</button><br />
+        <button onClick={editarPost} >Atualizar Post</button>
+
+        <ul>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>
+                <strong>ID: {post.id}</strong> <br />
+                <span>Titulo: {post.titulo}</span> <br />
+                <span>Autor: {post.autor}</span> <br />
+                <button onClick={()=> excluirPost(post.id)} >Excluir</button> <br /><br />
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ firebase/src/firebaseConnection.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUzCsCkzYX49dzUt8HFK8G0W1vpHUH4vM",
+    authDomain: "curso-8838e.firebaseapp.com",
+    projectId: "curso-8838e",
+    storageBucket: "curso-8838e.appspot.com",
+    messagingSenderId: "172725761908",
+    appId: "1:172725761908:web:46ff980ff6282cafb51e21",
+    measurementId: "G-RN0F7ERJHL"
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig)
+  const db = getFirestore(firebaseApp)
+
+  export {db}
